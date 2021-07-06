e402122896e6bf2ea89ccc4066c6074fdbbc7e7c
VitaliyM <vitalij-minenko@yandex.ru>
2021-07-06 22:24:34 +0500

    JRTB-3: added Command pattern for handling Telegram Bot commands

---
+++ src/test/java/command/HelpCommandTest.java
@@ -0,0 +1,31 @@
+package command;
+
+
+import com.github.javarushcommunity.jrtb.command.Command;
+import com.github.javarushcommunity.jrtb.command.HelpCommand;
+import com.github.javarushcommunity.jrtb.command.NoCommand;
+import org.junit.jupiter.api.DisplayName;
+
+import static com.github.javarushcommunity.jrtb.command.CommandName.HELP;
+import static com.github.javarushcommunity.jrtb.command.HelpCommand.HELP_MESSAGE;
+
+@DisplayName("Unit-level testing for HelpCommand")
+public class HelpCommandTest extends AbstractCommandTest {
+
+    @Override
+    String getCommandName() {
+        return HELP.getCommandName();
+    }
+
+    @Override
+    String getCommandMessage() {
+        return HELP_MESSAGE;
+    }
+
+    @Override
+    Command getCommand() {
+        return new HelpCommand(sendBotMessageService);
+    }
+
+
+}
