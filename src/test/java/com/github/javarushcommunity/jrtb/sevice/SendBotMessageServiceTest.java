7d87b392753b11a54efbf8e033378e7716b664f8
VitaliyM <vitalij-minenko@yandex.ru>
2021-07-31 00:47:35 +0500

    STEP_6 JRTB-5: added ability to subscribe on group JRTB-6: added ability to see the list of the group subscription.

--- src/test/java/com/github/javarushcommunity/jrtb/sevice/SendBotMessageServiceTest.java
+++ src/test/java/com/github/javarushcommunity/jrtb/sevice/SendBotMessageServiceTest.java
@@ -25,12 +25,12 @@ public class SendBotMessageServiceTest {
     @Test
     public void shouldProperlySendMessage() throws TelegramApiException {
         //given
-        Long chatId = Long.valueOf("test_chat_id");
+        Long chatId = 123L;
         String message = "test_message";
 
         SendMessage sendMessage = new SendMessage();
         sendMessage.setText(message);
-        sendMessage.setChatId(String.valueOf(chatId));
+        sendMessage.setChatId(chatId.toString());
         sendMessage.enableHtml(true);
 
         //when
