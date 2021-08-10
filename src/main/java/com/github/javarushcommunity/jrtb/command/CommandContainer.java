af9b889660b8fea0f66d6222a3897e9fda261fc3
Minenko-V-S <47560851+Minenko-V-S@users.noreply.github.com>
2021-08-11 00:01:01 +0500

    STEP_9_JRTB-10: fixed tests (#24)
    STEP_9_JRTB-10:extended statistic for admins.
    
    Co-authored-by: VitaliyM <vitalij-minenko@yandex.ru>

--- src/main/java/com/github/javarushcommunity/jrtb/command/CommandContainer.java
+++ src/main/java/com/github/javarushcommunity/jrtb/command/CommandContainer.java
@@ -1,12 +1,17 @@
 package com.github.javarushcommunity.jrtb.command;
 
+import com.github.javarushcommunity.jrtb.command.annotation.AdminCommand;
 import com.github.javarushcommunity.jrtb.javarushclient.JavaRushGroupClient;
-import com.github.javarushcommunity.jrtb.service.SendBotMessageService;
-import com.github.javarushcommunity.jrtb.service.TelegramUserService;
 import com.github.javarushcommunity.jrtb.service.GroupSubService;
+import com.github.javarushcommunity.jrtb.service.SendBotMessageService;
+import com.github.javarushcommunity.jrtb.service.StatisticsService;
+import com.github.javarushcommunity.jrtb.service.TelegramUserService;
 import com.google.common.collect.ImmutableMap;
 
+import java.util.List;
+
 import static com.github.javarushcommunity.jrtb.command.CommandName.*;
+import static java.util.Objects.nonNull;
 
 /**
  * Container of the {@link Command}s, which are using for handling telegram commands.
@@ -15,26 +20,40 @@ public class CommandContainer {
 
     private final ImmutableMap<String, Command> commandMap;
     private final Command unknownCommand;
+    private final List<String> admins;
 
     public CommandContainer(SendBotMessageService sendBotMessageService, TelegramUserService telegramUserService,
-                            JavaRushGroupClient javaRushGroupClient, GroupSubService groupSubService) {
+                            JavaRushGroupClient javaRushGroupClient, GroupSubService groupSubService, List<String> admins, StatisticsService statisticsService) {
+        this.admins = admins;
 
         commandMap = ImmutableMap.<String, Command>builder()
                 .put(CommandName.START.getCommandName(), new StartCommand(sendBotMessageService, telegramUserService))
                 .put(CommandName.STOP.getCommandName(), new StopCommand(sendBotMessageService, telegramUserService))
                 .put(CommandName.HELP.getCommandName(), new HelpCommand(sendBotMessageService))
                 .put(CommandName.NO.getCommandName(), new NoCommand(sendBotMessageService))
+                .put(ADMIN_HELP.getCommandName(), new AdminHelpCommand(sendBotMessageService))
                 .put(DELETE_GROUP_SUB.getCommandName(), new DeleteGroupSubCommand(sendBotMessageService, groupSubService, telegramUserService))
                 .put(LIST_GROUP_SUB.getCommandName(), new ListGroupSubCommand(sendBotMessageService, telegramUserService))
                 .put(ADD_GROUP_SUB.getCommandName(), new AddGroupSubCommand(sendBotMessageService, javaRushGroupClient, groupSubService))
-                .put(CommandName.STAT.getCommandName(), new StatCommand(sendBotMessageService, telegramUserService))
+                .put(CommandName.STAT.getCommandName(), new StatCommand(sendBotMessageService, statisticsService))
                 .build();
 
         unknownCommand = new UnknownCommand(sendBotMessageService);
     }
 
-    public Command retrieveCommand(String commandIdentifier) {
-        return commandMap.getOrDefault(commandIdentifier, unknownCommand);
+    public Command findCommand(String commandIdentifier, String username) {
+        Command orDefault = commandMap.getOrDefault(commandIdentifier, unknownCommand);
+        if (isAdminCommand(orDefault)) {
+            if (admins.contains(username)) {
+                return orDefault;
+            } else {
+                return unknownCommand;
+            }
+        }
+        return orDefault;
     }
 
+private boolean isAdminCommand(Command command){
+        return nonNull(command.getClass().getAnnotation(AdminCommand.class));
+}
 }
--- src/main/java/com/github/javarushcommunity/jrtb/command/CommandName.java
+++ src/main/java/com/github/javarushcommunity/jrtb/command/CommandName.java
@@ -8,6 +8,7 @@ public enum CommandName {
     START("/start"),
     STOP("/stop"),
     HELP("/help"),
+    ADMIN_HELP("/ahelp"),
     STAT("/stat"),
     DELETE_GROUP_SUB("/deletegroupsub"),
     ADD_GROUP_SUB("/addgroupsub"),
